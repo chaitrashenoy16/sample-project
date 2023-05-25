510dbec19daa918dcadc2fd41d78446cd40847c2
ESOLUTIONS\cshenoy <chaitra.shenoy@Trivium-esolutions.com>
2023-05-25 13:34:09 +0530

    Add project files.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./Component/Message"
+
+function App() {
+  return (<div><Message></Message></div>);
+}
+
+export default App;
---
+++ src/Component/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+    const name = "chaitra";
+    if(name)
+    return <h1>hello welcome to react application: {name}</h1>
+    return <h1>hello welcome to react application</h1>
+}
+
+export default Message;
